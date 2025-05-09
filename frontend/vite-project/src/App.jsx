316c0abdd00de35655a6d5661de3e7e887d17686
van son <vanson16032003@gmailcom>
2025-05-10 00:07:58 +0700

    delete folder

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -3,8 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
 import NewPost from "./page/NewPost";
 import EditPost from "./page/EditPost";
-import CreateComment from "./page/CreateComment";
-import EditComment from "./page/EditComment";
 import Post from "./page/Post";
 
 export default function App() {
@@ -15,8 +13,6 @@ export default function App() {
 
             <Route path="/new-post" element={<NewPost />} />
             <Route path="/edit-post/:id" element={<EditPost />} />
-            <Route path="/create-comment/:id" element={<CreateComment />} />
-            <Route path="/edit-comment/:id" element={<EditComment />} />
         </Routes>
     );
 }
